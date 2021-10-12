cc6363b23148a5c1f3876426f4ae678a9da594bf
aikozhaoz <aikozhao1203@gmail.com>
2021-10-12 18:18:08 -0400

    Created a memory class for  keep tracking of static, stack and heap.

--- Expr.java
+++ Expr.java
@@ -45,14 +45,4 @@ public class Expr {
         }
     }
 
-    public void print(int indent) {
-        term.print(indent);
-        if (option == 2) {
-            System.out.print("+");
-            expr.print(indent);
-        } else if (option == 3) {
-            System.out.print("-");
-            expr.print(indent);
-        }
-    }
 }
